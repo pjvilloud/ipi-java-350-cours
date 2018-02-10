499dee452fdf4ddbfcb901a117da8706da613e61
pijey <pjvilloud-bitbucket@yahoo.fr>
2018-02-10 18:20:33 +0100

    Ajout Junit & co

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
-var imp = impress();
-imp.init();
+
 hljs.initHighlightingOnLoad();
 var nbSlides = $(".step.slide").length;
 var showPopover = new URL(window.location).searchParams.get("showPopover");
@@ -16,15 +15,15 @@ rootElement.addEventListener( "impress:stepenter", function(event) {
   $("#progress").attr("aria-valuenow", percentageSlide);
 
   if(showPopover === "true") {
-  	setTimeout(function(){
-	  	$('#' + currentStep.id + ' [data-toggle="popover"]').not('.fun').popover('show', {
-		    container: 'body'
-		  });
-	}, 1000);
+    setTimeout(function(){
+      $('#' + currentStep.id + ' [data-toggle="popover"]').popover('show', {
+        container: 'body'
+      });
+  }, 1000);
   } else {
-  	$('#' + currentStep.id + ' [data-toggle="popover"]').popover({
-	    container: 'body'
-	  });
+    $('#' + currentStep.id + ' [data-toggle="popover"]').popover({
+      container: 'body'
+    });
   }
 
   
@@ -43,26 +42,17 @@ $(window).on('hashchange', function(e){
     newHash = origEvent.newURL.substring(origEvent.newURL.lastIndexOf("#")).replace("/","");
     $('#my-navbar li a').removeClass("active");
     $('#my-navbar li a[href="' + newHash.substring(0,newHash.indexOf("-")) + '"]').addClass("active");
-    $('#my-navbar li a[href="' + newHash.substring(0,newHash.lastIndexOf("-")) + '"]').addClass("active");
+    urls = newHash.split("-");
+    if(urls.length > 3){
+      $('#my-navbar li a[href="' + urls[0] + "-" + urls[1] + '"]').addClass("active");
+    } else {
+      $('#my-navbar li a[href="' + newHash.substring(0,newHash.lastIndexOf("-")) + '"]').addClass("active");
+    }
     $('#my-navbar li a[href="' + newHash + '"]').addClass("active");
     
 });
 
-$(window).on('mousewheel', function(event) {
-  if(event.deltaY < 0) {
-    imp.next();
-  } else{
-    imp.prev();
-  }
-  e.preventDefault();
-});
-
 //-------------------
-$("#curdate").html(moment().format("YYYY-MM-DD"));
-$("#curtime").html(moment().format("HH-mm-ss"));
-$("#curyear").html(moment().format("YYYY"));
-$("#curmonth").html(moment().format("MM"));
-$("#curday").html(moment().format("DD"));
 
 $("#correctGeneric").click(function(event) {
   $(event.target).toggleClass('btn-primary').toggleClass('btn-success');
@@ -81,3 +71,52 @@ $("#toggleClassIntern").click(function(event) {
   var html = $("#internePrivate").html();
   $("#internePrivate").html(html === '<span class="hljs-keyword">private</span>' ? '<span class="hljs-keyword">public</span>' : '<span class="hljs-keyword">private</span>');
 });
+
+$("div.step.slide").each(function(index, el) {
+  var id = $(el).attr("id");
+  var title = $(el).find("h1.display-3").text();
+  $(el).attr("data-nb", index + 1);
+  var yOffset = 1100;
+  var xOffset = 2000;
+  var ybase = 1100;
+  var xbase = 0; 
+  if(id !== 'accueil'){
+    if(id.indexOf("-") > 0){
+      if(id.lastIndexOf("-") != id.indexOf("-")){
+        if(id.split("-").length == 4){
+          $(el).attr("data-rel-x", xOffset);
+          $(el).attr("data-rel-y", 0);
+          $(el).attr("data-rotate-x", 90);
+          $(el).attr("data-z", -2000);
+        } else {
+          $(el).attr("data-rel-x", 0);
+          $(el).attr("data-rel-y", 0);
+          $(el).attr("data-z", -2000);
+          $(el).attr("data-rotate-x", 90);
+        }
+      } else {
+        var baseId = id.substring(0,id.indexOf("-"));
+        console.log(baseId);
+        $("div#dropdown-"+baseId).append('<a class="dropdown-item" href="#'+id+'">'+title+'</a>');
+        $(el).attr("data-rel-x", xOffset);
+        $(el).attr("data-rel-y", 0);
+        $(el).attr("data-z", 0);
+      }
+    } else {
+      if($('div[id^="'+id+'-"]').length > 0){
+        $("ul.nav.nav-pills.mr-auto").append('<li class="nav-item dropdown"> <a class="nav-link dropdown-toggle" id="navbarDropdown'+id+'" href="#'+id+'" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">'+title+'</a> <div id="dropdown-'+id+'" class="dropdown-menu" aria-labelledby="navbarDropdown'+id+'"></div></li>');  
+        $(el).attr("data-x", 0);
+        $(el).attr("data-rel-y", yOffset);
+        $(el).attr("data-z", 0);
+      } else {
+        $("ul.nav.nav-pills.mr-auto").append('<li class="nav-item"><a class="nav-link" href="#'+id+'">'+title+'</a></li>');  
+        $(el).attr("data-x", 0);
+        $(el).attr("data-rel-y", yOffset);
+        $(el).attr("data-z", 0);
+      }
+    }
+  }
+});
+
+var imp = impress();
+imp.init();
